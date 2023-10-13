c18cd1ec5737a5f6382892433384a96aa29ee68e
folfisgit <147885008+folfisgit@users.noreply.github.com>
2023-10-13 23:14:55 +0100

    Add files via upload

---
+++ 5-1/Form1.Designer.vb
@@ -0,0 +1,415 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtDecimal = New TextBox()
+        lblDecimal = New Label()
+        btnTruncateRoundNumber = New Button()
+        txtTruncated = New TextBox()
+        cbProblems = New ComboBox()
+        lblTruncated = New Label()
+        lblRounded = New Label()
+        txtRounded = New TextBox()
+        Label1 = New Label()
+        btnClear = New Button()
+        txtStringToNum = New TextBox()
+        txtCountedChars = New TextBox()
+        btnCountChars = New Button()
+        lblStringToNum = New Label()
+        lblNumOfChars = New Label()
+        txtCharInput = New TextBox()
+        txtASCIIOut = New TextBox()
+        btnCharToASCII = New Button()
+        lblCharInput = New Label()
+        lblASCIIOut = New Label()
+        txtASCIIInput = New TextBox()
+        txtCharOut = New TextBox()
+        lblASCIIInput = New Label()
+        lblCharOut = New Label()
+        btnASCIItoChar = New Button()
+        txtSurname = New TextBox()
+        btnUpperCaser = New Button()
+        txtSurnameUpper = New TextBox()
+        lblSurname = New Label()
+        lblSurnameUpper = New Label()
+        SuspendLayout()
+        ' 
+        ' txtDecimal
+        ' 
+        txtDecimal.Location = New Point(151, 219)
+        txtDecimal.Name = "txtDecimal"
+        txtDecimal.Size = New Size(100, 23)
+        txtDecimal.TabIndex = 0
+        txtDecimal.Visible = False
+        ' 
+        ' lblDecimal
+        ' 
+        lblDecimal.AutoSize = True
+        lblDecimal.Location = New Point(118, 201)
+        lblDecimal.Name = "lblDecimal"
+        lblDecimal.Size = New Size(169, 15)
+        lblDecimal.TabIndex = 1
+        lblDecimal.Text = "Enter a Number with decimals:"
+        lblDecimal.Visible = False
+        ' 
+        ' btnTruncateRoundNumber
+        ' 
+        btnTruncateRoundNumber.Enabled = False
+        btnTruncateRoundNumber.Location = New Point(360, 219)
+        btnTruncateRoundNumber.Name = "btnTruncateRoundNumber"
+        btnTruncateRoundNumber.Size = New Size(75, 23)
+        btnTruncateRoundNumber.TabIndex = 2
+        btnTruncateRoundNumber.Text = "Calculate"
+        btnTruncateRoundNumber.UseVisualStyleBackColor = True
+        btnTruncateRoundNumber.Visible = False
+        ' 
+        ' txtTruncated
+        ' 
+        txtTruncated.Location = New Point(552, 193)
+        txtTruncated.Name = "txtTruncated"
+        txtTruncated.ReadOnly = True
+        txtTruncated.Size = New Size(100, 23)
+        txtTruncated.TabIndex = 3
+        txtTruncated.Visible = False
+        ' 
+        ' cbProblems
+        ' 
+        cbProblems.FormattingEnabled = True
+        cbProblems.Items.AddRange(New Object() {"Numeric Truncation/Rounding", "String Character Counter", "Char to ASCII", "ASCII to Char", "Uppercase Conversion"})
+        cbProblems.Location = New Point(342, 78)
+        cbProblems.Name = "cbProblems"
+        cbProblems.Size = New Size(121, 23)
+        cbProblems.TabIndex = 4
+        ' 
+        ' lblTruncated
+        ' 
+        lblTruncated.AutoSize = True
+        lblTruncated.Location = New Point(555, 175)
+        lblTruncated.Name = "lblTruncated"
+        lblTruncated.Size = New Size(93, 15)
+        lblTruncated.TabIndex = 5
+        lblTruncated.Text = "Truncated Input:"
+        lblTruncated.Visible = False
+        ' 
+        ' lblRounded
+        ' 
+        lblRounded.AutoSize = True
+        lblRounded.Location = New Point(558, 240)
+        lblRounded.Name = "lblRounded"
+        lblRounded.Size = New Size(89, 15)
+        lblRounded.TabIndex = 7
+        lblRounded.Text = "Rounded Input:"
+        lblRounded.Visible = False
+        ' 
+        ' txtRounded
+        ' 
+        txtRounded.Location = New Point(552, 258)
+        txtRounded.Name = "txtRounded"
+        txtRounded.ReadOnly = True
+        txtRounded.Size = New Size(100, 23)
+        txtRounded.TabIndex = 6
+        txtRounded.Visible = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(358, 60)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(89, 15)
+        Label1.TabIndex = 8
+        Label1.Text = "Pick a Problem:"
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        btnClear.Location = New Point(347, 366)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(102, 43)
+        btnClear.TabIndex = 9
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' txtStringToNum
+        ' 
+        txtStringToNum.Location = New Point(151, 221)
+        txtStringToNum.Name = "txtStringToNum"
+        txtStringToNum.Size = New Size(100, 23)
+        txtStringToNum.TabIndex = 10
+        txtStringToNum.Visible = False
+        ' 
+        ' txtCountedChars
+        ' 
+        txtCountedChars.Location = New Point(552, 222)
+        txtCountedChars.Name = "txtCountedChars"
+        txtCountedChars.ReadOnly = True
+        txtCountedChars.Size = New Size(100, 23)
+        txtCountedChars.TabIndex = 12
+        txtCountedChars.Visible = False
+        ' 
+        ' btnCountChars
+        ' 
+        btnCountChars.Enabled = False
+        btnCountChars.Location = New Point(360, 220)
+        btnCountChars.Name = "btnCountChars"
+        btnCountChars.Size = New Size(75, 23)
+        btnCountChars.TabIndex = 13
+        btnCountChars.Text = "Count"
+        btnCountChars.UseVisualStyleBackColor = True
+        btnCountChars.Visible = False
+        ' 
+        ' lblStringToNum
+        ' 
+        lblStringToNum.AutoSize = True
+        lblStringToNum.Location = New Point(171, 203)
+        lblStringToNum.Name = "lblStringToNum"
+        lblStringToNum.Size = New Size(61, 15)
+        lblStringToNum.TabIndex = 14
+        lblStringToNum.Text = "Enter Text:"
+        lblStringToNum.Visible = False
+        ' 
+        ' lblNumOfChars
+        ' 
+        lblNumOfChars.AutoSize = True
+        lblNumOfChars.Location = New Point(539, 204)
+        lblNumOfChars.Name = "lblNumOfChars"
+        lblNumOfChars.Size = New Size(127, 15)
+        lblNumOfChars.TabIndex = 15
+        lblNumOfChars.Text = "Number of Characters:"
+        lblNumOfChars.Visible = False
+        ' 
+        ' txtCharInput
+        ' 
+        txtCharInput.Location = New Point(151, 219)
+        txtCharInput.Name = "txtCharInput"
+        txtCharInput.Size = New Size(100, 23)
+        txtCharInput.TabIndex = 16
+        txtCharInput.Visible = False
+        ' 
+        ' txtASCIIOut
+        ' 
+        txtASCIIOut.Location = New Point(552, 221)
+        txtASCIIOut.Name = "txtASCIIOut"
+        txtASCIIOut.ReadOnly = True
+        txtASCIIOut.Size = New Size(100, 23)
+        txtASCIIOut.TabIndex = 17
+        txtASCIIOut.Visible = False
+        ' 
+        ' btnCharToASCII
+        ' 
+        btnCharToASCII.Enabled = False
+        btnCharToASCII.Location = New Point(360, 218)
+        btnCharToASCII.Name = "btnCharToASCII"
+        btnCharToASCII.Size = New Size(75, 23)
+        btnCharToASCII.TabIndex = 18
+        btnCharToASCII.Text = "Convert"
+        btnCharToASCII.UseVisualStyleBackColor = True
+        btnCharToASCII.Visible = False
+        ' 
+        ' lblCharInput
+        ' 
+        lblCharInput.AutoSize = True
+        lblCharInput.Location = New Point(155, 204)
+        lblCharInput.Name = "lblCharInput"
+        lblCharInput.Size = New Size(91, 15)
+        lblCharInput.TabIndex = 19
+        lblCharInput.Text = "Enter Character:"
+        lblCharInput.Visible = False
+        ' 
+        ' lblASCIIOut
+        ' 
+        lblASCIIOut.AutoSize = True
+        lblASCIIOut.Location = New Point(567, 204)
+        lblASCIIOut.Name = "lblASCIIOut"
+        lblASCIIOut.Size = New Size(69, 15)
+        lblASCIIOut.TabIndex = 20
+        lblASCIIOut.Text = "ASCII Code:"
+        lblASCIIOut.Visible = False
+        ' 
+        ' txtASCIIInput
+        ' 
+        txtASCIIInput.Location = New Point(151, 220)
+        txtASCIIInput.Name = "txtASCIIInput"
+        txtASCIIInput.Size = New Size(100, 23)
+        txtASCIIInput.TabIndex = 21
+        txtASCIIInput.Visible = False
+        ' 
+        ' txtCharOut
+        ' 
+        txtCharOut.Location = New Point(552, 222)
+        txtCharOut.Name = "txtCharOut"
+        txtCharOut.ReadOnly = True
+        txtCharOut.Size = New Size(100, 23)
+        txtCharOut.TabIndex = 22
+        txtCharOut.Visible = False
+        ' 
+        ' lblASCIIInput
+        ' 
+        lblASCIIInput.AutoSize = True
+        lblASCIIInput.Location = New Point(152, 204)
+        lblASCIIInput.Name = "lblASCIIInput"
+        lblASCIIInput.Size = New Size(99, 15)
+        lblASCIIInput.TabIndex = 23
+        lblASCIIInput.Text = "Enter ASCII Code:"
+        lblASCIIInput.Visible = False
+        ' 
+        ' lblCharOut
+        ' 
+        lblCharOut.AutoSize = True
+        lblCharOut.Location = New Point(575, 204)
+        lblCharOut.Name = "lblCharOut"
+        lblCharOut.Size = New Size(61, 15)
+        lblCharOut.TabIndex = 24
+        lblCharOut.Text = "Character:"
+        lblCharOut.Visible = False
+        ' 
+        ' btnASCIItoChar
+        ' 
+        btnASCIItoChar.Enabled = False
+        btnASCIItoChar.Location = New Point(360, 218)
+        btnASCIItoChar.Name = "btnASCIItoChar"
+        btnASCIItoChar.Size = New Size(75, 23)
+        btnASCIItoChar.TabIndex = 25
+        btnASCIItoChar.Text = "Convert"
+        btnASCIItoChar.UseVisualStyleBackColor = True
+        btnASCIItoChar.Visible = False
+        ' 
+        ' txtSurname
+        ' 
+        txtSurname.Location = New Point(151, 222)
+        txtSurname.Name = "txtSurname"
+        txtSurname.Size = New Size(100, 23)
+        txtSurname.TabIndex = 26
+        txtSurname.Visible = False
+        ' 
+        ' btnUpperCaser
+        ' 
+        btnUpperCaser.Enabled = False
+        btnUpperCaser.Location = New Point(360, 221)
+        btnUpperCaser.Name = "btnUpperCaser"
+        btnUpperCaser.Size = New Size(75, 23)
+        btnUpperCaser.TabIndex = 27
+        btnUpperCaser.Text = "Convert"
+        btnUpperCaser.UseVisualStyleBackColor = True
+        btnUpperCaser.Visible = False
+        ' 
+        ' txtSurnameUpper
+        ' 
+        txtSurnameUpper.Location = New Point(552, 222)
+        txtSurnameUpper.Name = "txtSurnameUpper"
+        txtSurnameUpper.ReadOnly = True
+        txtSurnameUpper.Size = New Size(100, 23)
+        txtSurnameUpper.TabIndex = 28
+        txtSurnameUpper.Visible = False
+        ' 
+        ' lblSurname
+        ' 
+        lblSurname.AutoSize = True
+        lblSurname.Location = New Point(157, 204)
+        lblSurname.Name = "lblSurname"
+        lblSurname.Size = New Size(87, 15)
+        lblSurname.TabIndex = 29
+        lblSurname.Text = "Enter Surname:"
+        lblSurname.Visible = False
+        ' 
+        ' lblSurnameUpper
+        ' 
+        lblSurnameUpper.AutoSize = True
+        lblSurnameUpper.Location = New Point(545, 204)
+        lblSurnameUpper.Name = "lblSurnameUpper"
+        lblSurnameUpper.Size = New Size(115, 15)
+        lblSurnameUpper.TabIndex = 30
+        lblSurnameUpper.Text = "Converted Surname:"
+        lblSurnameUpper.Visible = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(lblSurnameUpper)
+        Controls.Add(lblSurname)
+        Controls.Add(txtSurnameUpper)
+        Controls.Add(btnUpperCaser)
+        Controls.Add(txtSurname)
+        Controls.Add(btnASCIItoChar)
+        Controls.Add(lblCharOut)
+        Controls.Add(lblASCIIInput)
+        Controls.Add(txtCharOut)
+        Controls.Add(txtASCIIInput)
+        Controls.Add(lblASCIIOut)
+        Controls.Add(lblCharInput)
+        Controls.Add(btnCharToASCII)
+        Controls.Add(txtASCIIOut)
+        Controls.Add(txtCharInput)
+        Controls.Add(lblNumOfChars)
+        Controls.Add(lblStringToNum)
+        Controls.Add(btnCountChars)
+        Controls.Add(txtCountedChars)
+        Controls.Add(txtStringToNum)
+        Controls.Add(btnClear)
+        Controls.Add(Label1)
+        Controls.Add(lblRounded)
+        Controls.Add(txtRounded)
+        Controls.Add(lblTruncated)
+        Controls.Add(cbProblems)
+        Controls.Add(txtTruncated)
+        Controls.Add(btnTruncateRoundNumber)
+        Controls.Add(lblDecimal)
+        Controls.Add(txtDecimal)
+        Name = "Form1"
+        Text = "5-1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtDecimal As TextBox
+    Friend WithEvents lblDecimal As Label
+    Friend WithEvents btnTruncateRoundNumber As Button
+    Friend WithEvents txtTruncated As TextBox
+    Friend WithEvents cbProblems As ComboBox
+    Friend WithEvents lblTruncated As Label
+    Friend WithEvents lblRounded As Label
+    Friend WithEvents txtRounded As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnClear As Button
+    Friend WithEvents txtStringToNum As TextBox
+    Friend WithEvents txtCountedChars As TextBox
+    Friend WithEvents btnCountChars As Button
+    Friend WithEvents lblStringToNum As Label
+    Friend WithEvents lblNumOfChars As Label
+    Friend WithEvents txtCharInput As TextBox
+    Friend WithEvents txtASCIIOut As TextBox
+    Friend WithEvents btnCharToASCII As Button
+    Friend WithEvents lblCharInput As Label
+    Friend WithEvents lblASCIIOut As Label
+    Friend WithEvents txtASCIIInput As TextBox
+    Friend WithEvents txtCharOut As TextBox
+    Friend WithEvents lblASCIIInput As Label
+    Friend WithEvents lblCharOut As Label
+    Friend WithEvents btnASCIItoChar As Button
+    Friend WithEvents txtSurname As TextBox
+    Friend WithEvents btnUpperCaser As Button
+    Friend WithEvents txtSurnameUpper As TextBox
+    Friend WithEvents lblSurname As Label
+    Friend WithEvents lblSurnameUpper As Label
+End Class
---
+++ 5-1/Form1.vb
@@ -0,0 +1,229 @@
+﻿Public Class Form1
+    Private Function Truncate(value As Decimal, numberofDigits As Integer) As Decimal
+        ' create function that truncates input value, by the amount of digits to truncate by
+        Dim multiplier As Decimal = CDec(Math.Pow(10, numberofDigits))
+        ' multiplier 10 to the power of 10
+        Dim truncatedValue As Decimal = Decimal.Truncate(value * multiplier) / multiplier
+        ' truncate the value and return
+        Return truncatedValue
+    End Function
+    Private Function ContainsNumber(input As String) As Boolean
+        ' check if a string contains a number
+        Return input.Any(Function(c) Char.IsDigit(c))
+    End Function
+    Private Function RoundToTwoDecimalPlaces(value As Decimal) As Decimal
+        ' round a decimal number to two decimal places
+        Return Math.Round(value, 2)
+    End Function
+    Private Sub cbProblems_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbProblems.SelectedIndexChanged
+        ' handle combo box selection change
+        If cbProblems.SelectedItem = "Numeric Truncation/Rounding" Then
+            ' show/hide controls based on combo box selection
+            lblDecimal.Visible = True
+            lblRounded.Visible = True
+            lblTruncated.Visible = True
+            txtDecimal.Visible = True
+            txtRounded.Visible = True
+            txtTruncated.Visible = True
+            btnTruncateRoundNumber.Visible = True
+        Else
+            ' hide numeric truncation/rounding controls
+            lblDecimal.Visible = False
+            lblRounded.Visible = False
+            lblTruncated.Visible = False
+            txtDecimal.Visible = False
+            txtRounded.Visible = False
+            txtTruncated.Visible = False
+            btnTruncateRoundNumber.Visible = False
+        End If
+        If cbProblems.SelectedItem = "String Character Counter" Then
+            ' show string character counter controls
+            lblNumOfChars.Visible = True
+            lblStringToNum.Visible = True
+            txtCountedChars.Visible = True
+            txtStringToNum.Visible = True
+            btnCountChars.Visible = True
+        Else
+            ' hide string character counter controls
+            lblNumOfChars.Visible = False
+            lblStringToNum.Visible = False
+            txtCountedChars.Visible = False
+            txtStringToNum.Visible = False
+            btnCountChars.Visible = False
+        End If
+        If cbProblems.SelectedItem = "Char to ASCII" Then
+            ' show char to ascii controls
+            lblASCIIOut.Visible = True
+            lblCharInput.Visible = True
+            txtASCIIOut.Visible = True
+            txtCharInput.Visible = True
+            btnCharToASCII.Visible = True
+        Else
+            ' hide char to ascii controls
+            lblASCIIOut.Visible = False
+            lblCharInput.Visible = False
+            txtASCIIOut.Visible = False
+            txtCharInput.Visible = False
+            btnCharToASCII.Visible = False
+        End If
+        If cbProblems.SelectedItem = "ASCII to Char" Then
+            ' show ascii to char controls
+            lblASCIIInput.Visible = True
+            lblCharOut.Visible = True
+            btnASCIItoChar.Visible = True
+            txtASCIIInput.Visible = True
+            txtCharOut.Visible = True
+        Else
+            ' hide ascii to char controls
+            lblASCIIInput.Visible = False
+            lblCharOut.Visible = False
+            btnASCIItoChar.Visible = False
+            txtASCIIInput.Visible = False
+            txtCharOut.Visible = False
+        End If
+        If cbProblems.SelectedItem = "Uppercase Conversion" Then
+            ' show uppercase conversion controls
+            lblSurname.Visible = True
+            lblSurnameUpper.Visible = True
+            txtSurname.Visible = True
+            txtSurnameUpper.Visible = True
+            btnUpperCaser.Visible = True
+        Else
+            ' hide uppercase conversion controls
+            lblSurname.Visible = False
+            lblSurnameUpper.Visible = False
+            txtSurname.Visible = False
+            txtSurnameUpper.Visible = False
+            btnUpperCaser.Visible = False
+        End If
+    End Sub
+    Private Sub txtDecimal_TextChanged(sender As Object, e As EventArgs) Handles txtDecimal.TextChanged
+        ' handle text change in the decimal text box
+        If IsNumeric(txtDecimal.Text) Then
+            ' enable the truncate/round button
+            btnTruncateRoundNumber.Enabled = True
+        ElseIf txtDecimal.Text = "" Then
+            ' disable the truncate/round button
+            btnTruncateRoundNumber.Enabled = False
+        Else
+            btnTruncateRoundNumber.Enabled = False
+            MessageBox.Show($"You must enter a decimal number.")
+            ' show an error message
+        End If
+    End Sub
+    Private Sub btnTruncateRoundNumber_Click(sender As Object, e As EventArgs) Handles btnTruncateRoundNumber.Click
+        ' handle click on the truncate/round button
+        If IsNumeric(txtDecimal.Text) Then
+            Dim DecimalNum As Decimal = Convert.ToDecimal(txtDecimal.Text)
+            ' convert the text to a decimal number
+            Dim numberOfDigits As Integer = 2
+            ' set the number of digits to truncate
+            Dim truncatedValue As Decimal = Truncate(DecimalNum, numberOfDigits)
+            ' truncate the value and display in the text box
+            txtTruncated.Text = truncatedValue.ToString()
+            Dim roundedValue As Decimal = RoundToTwoDecimalPlaces(DecimalNum)
+            ' round the value to two decimal places and display in the text box
+            txtRounded.Text = roundedValue.ToString()
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' handle click on the clear button
+        ' clear all text boxes and disable buttons
+        txtDecimal.Text = Nothing
+        txtRounded.Text = Nothing
+        txtTruncated.Text = Nothing
+        txtCountedChars.Text = Nothing
+        txtStringToNum.Text = Nothing
+        txtCharInput.Text = Nothing
+        txtASCIIOut.Text = Nothing
+        txtASCIIInput.Text = Nothing
+        txtCharOut.Text = Nothing
+        txtSurnameUpper.Text = Nothing
+        txtSurname.Text = Nothing
+        btnCharToASCII.Enabled = False
+        btnTruncateRoundNumber.Enabled = False
+        btnCountChars.Enabled = False
+        btnASCIItoChar.Enabled = False
+        btnUpperCaser.Enabled = False
+    End Sub
+    Private Sub txtStringToNum_TextChanged(sender As Object, e As EventArgs) Handles txtStringToNum.TextChanged
+        ' handle text change in the string to number text box
+        btnCountChars.Enabled = True
+        ' enable the character count button if the text is not empty
+        If txtStringToNum.Text = "" Then
+            btnCountChars.Enabled = False
+            ' disable the character count button
+        End If
+    End Sub
+    Private Sub btnCountChars_Click(sender As Object, e As EventArgs) Handles btnCountChars.Click
+        ' handle click on the character count button
+        Dim inputText As String = txtStringToNum.Text
+        ' count characters in the input text and display in the text box
+        Dim characterCount As Integer = inputText.Length
+        txtCountedChars.Text = Convert.ToString(characterCount)
+    End Sub
+    Private Sub txtCharInput_TextChanged(sender As Object, e As EventArgs) Handles txtCharInput.TextChanged
+        ' handle text change in the character input text box
+        If txtCharInput.TextLength = 1 Then
+            btnCharToASCII.Enabled = True
+            ' enable the char to ascii button
+        ElseIf txtCharInput.Text = "" Then
+            btnCharToASCII.Enabled = False
+            ' disable the char to ascii button
+        Else
+            btnCharToASCII.Enabled = False
+        End If
+    End Sub
+    Private Sub btnCharToASCII_Click(sender As Object, e As EventArgs) Handles btnCharToASCII.Click
+        ' handle click on the char to ascii button
+        Dim myChar As Char = Convert.ToChar(txtCharInput.Text)
+        Dim asciiCode As Integer = Asc(myChar)
+        ' convert the character to its ascii code and display
+        txtASCIIOut.Text = asciiCode
+    End Sub
+    Private Sub btnASCIItoChar_Click(sender As Object, e As EventArgs) Handles btnASCIItoChar.Click
+        ' handle click on the ascii to char button
+        If Integer.TryParse(txtASCIIInput.Text, Nothing) Then
+            Dim asciiValue As Integer = Integer.Parse(txtASCIIInput.Text)
+            Dim convertedChar As Char = Convert.ToChar(asciiValue)
+            ' convert the ascii value to a character and display
+            txtCharOut.Text = Convert.ToString(convertedChar)
+        End If
+    End Sub
+    Private Sub txtASCIIInput_TextChanged(sender As Object, e As EventArgs) Handles txtASCIIInput.TextChanged
+        ' handle text change in the ascii input text box
+        If Integer.TryParse(txtASCIIInput.Text, Nothing) Then
+            ' enable the ascii to char button
+            btnASCIItoChar.Enabled = True
+        ElseIf txtASCIIInput.Text = "" Then
+            ' disable the ascii to char button
+            btnASCIItoChar.Enabled = False
+        Else
+            btnASCIItoChar.Enabled = False
+            MessageBox.Show("Please enter a valid ASCII value (an integer).")
+            ' show an error message
+        End If
+    End Sub
+    Private Sub btnUpperCaser_Click(sender As Object, e As EventArgs) Handles btnUpperCaser.Click
+        ' handle click on the uppercase conversion button
+        Dim inputText As String = txtSurname.Text
+        Dim uppercasedText As String = inputText.ToUpper()
+        ' convert the text to uppercase and display
+        txtSurnameUpper.Text = uppercasedText
+    End Sub
+    Private Sub txtSurname_TextChanged(sender As Object, e As EventArgs) Handles txtSurname.TextChanged
+        ' handle text change in the surname text box
+        Dim hasNumber As Boolean = ContainsNumber(txtSurname.Text)
+        ' check if the text contains a number
+        If txtSurname.Text = "" Then
+            btnUpperCaser.Enabled = False
+            ' disable the uppercase conversion button
+        ElseIf hasNumber Then
+            btnUpperCaser.Enabled = False
+        Else
+            btnUpperCaser.Enabled = True
+            ' enable the uppercase conversion button
+        End If
+    End Sub
+End Class
+
